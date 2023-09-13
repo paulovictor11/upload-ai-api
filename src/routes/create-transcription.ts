28ffcc7732512be1ac72b34b094c5fd383057a82
paulovictor11 <pauloalmeidajp652@gmail.com>
2023-09-12 22:31:09 -0300

    feat: :sparkles: aula-1
    Aula 1 finalizada. Criando backend da aplicação.

---
+++ src/routes/create-transcription.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+import { createReadStream } from "node:fs";
+
+export async function createTranscriptionRoute(app: FastifyInstance) {
+    app.post("/videos/:videoId/transcription", async (request) => {
+        const paramsSchema = z.object({
+            videoId: z.string().uuid(),
+        });
+
+        const { videoId } = paramsSchema.parse(request.params);
+
+        const bodySchema = z.object({
+            prompt: z.string(),
+        });
+
+        const { prompt } = bodySchema.parse(request.body);
+
+        const video = await prisma.video.findUniqueOrThrow({
+            where: {
+                id: videoId,
+            },
+        });
+
+        const audioReadStream = createReadStream(video.path);
+        const response = await openai.audio.transcriptions.create({
+            file: audioReadStream,
+            model: "whisper-1",
+            language: "pt",
+            response_format: "json",
+            temperature: 0,
+            prompt,
+        });
+
+        await prisma.video.update({
+            where: {
+                id: videoId,
+            },
+            data: {
+                transcription: response.text,
+            },
+        });
+
+        return {
+            transcription: response.text,
+        };
+    });
+}
