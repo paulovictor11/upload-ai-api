28d9c564bd32fdc871f26555ad0494e2333996f2
paulovictor11 <pauloalmeidajp652@gmail.com>
2023-09-13 22:19:40 -0300

    feat(aula-2): :sparkles: Adiconando pacote AI
    Adicionando SDK AI da vercel para gerar os resultados

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -2,18 +2,17 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
+import { streamToResponse, OpenAIStream } from "ai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
     app.post("/ai/complete", async (request, reply) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5),
         });
 
-        const { videoId, template, temperature } = bodySchema.parse(
-            request.body
-        );
+        const { videoId, prompt, temperature } = bodySchema.parse(request.body);
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -27,7 +26,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
                 .send({ error: "Video transcription wat not generated yet." });
         }
 
-        const promptMessage = template.replace(
+        const promptMessage = prompt.replace(
             "{transcription}",
             video.transcription
         );
@@ -36,8 +35,17 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             model: "gpt-3.5-turbo-16k",
             temperature,
             messages: [{ role: "user", content: promptMessage }],
+            stream: true,
         });
 
-        return response;
+        const stream = OpenAIStream(response);
+
+        streamToResponse(stream, reply.raw, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods":
+                    "GET, POST, PUT, DELETE, OPTIONS",
+            },
+        });
     });
 }
